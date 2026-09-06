a3c6037feb8d5d08a6e8663e06f8ec0140819661
Eng. Abdulrhman Saad Aljradi <aljrady591@gmail.com>
2026-09-06 16:19:12 +0300

    chore: sync working tree state (deploy files, seeders, reports, settings/favicon, mobile fixes)

--- backend/resources/views/profile/edit.blade.php
+++ backend/resources/views/profile/edit.blade.php
@@ -19,7 +19,7 @@
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg" style="display: none" font-family: 'Segoe UI', sans-serif;>
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
